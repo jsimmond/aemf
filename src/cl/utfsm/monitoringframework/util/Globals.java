439cb70fb16ebe4c2000cb2bd024bd73efea9713
sebastian <sebastian@machine.vasquez>
2012-08-15 15:40:16 -0400

    Clases Global y Util creadas, sin nada dentro

---
+++ src/cl/utfsm/monitoringframework/util/Globals.java
@@ -0,0 +1,10 @@
+package cl.utfsm.monitoringframework.util;
+
+/**
+ * Global Class: Privides a set of numerous static values
+ * @author sebastian
+ *
+ */
+public class Globals {
+	
+}
---
+++ src/cl/utfsm/monitoringframework/util/Util.java
@@ -0,0 +1,10 @@
+package cl.utfsm.monitoringframework.util;
+
+/**
+ * Util Class: Privides a set of numerous static utilities
+ * @author sebastian
+ *
+ */
+public class Util {
+
+}
